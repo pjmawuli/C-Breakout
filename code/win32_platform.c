30d8a1f96229a80aaab5cbc121c36e58073d20b7
Padi Julius <87507466+pjmawuli@users.noreply.github.com>
2024-10-08 13:39:12 +0000

    Setup window creation

--- code/win32_platform.c
+++ code/win32_platform.c
@@ -1,5 +1,15 @@
 #include <windows.h>
 
+typedef int b32;
+
+#define true 1
+#define false 0
+
+#define global_variable static
+#define internal static
+
+global_variable b32 running = true;
+
 // Defining the Window Proc function to handle messages
 LRESULT CALLBACK window_callback (
   HWND window,
@@ -11,46 +21,44 @@ LRESULT CALLBACK window_callback (
   return DefWindowProcA(window, message, w_param, l_param);
 };
 
+// Main function for windows graphics applications
 int WinMain(
   HINSTANCE hInstance,
   HINSTANCE hPrevInstance,
   LPSTR     lpCmdLine,
   int       nShowCmd
 ) {
-  // First we register the window class
+
+  // Defining a window class
   WNDCLASSA window_class = {0};
   // Then we set some of its properties
-  window_class.style = CS_HREDRAW | CS_VREDRAW;
+  window_class.style = CS_HREDRAW | CS_VREDRAW; // Enables refresh when the window is resized horizontally and vertically
   window_class.lpszClassName = "Game_Window_Class";
   window_class.lpfnWndProc = window_callback;
-  /*
-  int       cbClsExtra;
-  int       cbWndExtra;
-  HINSTANCE hInstance;
-  HICON     hIcon;
-  HCURSOR   hCursor;
-  HBRUSH    hbrBackground;
-  LPCSTR    lpszMenuName;
-  */
+  window_class.hInstance = hInstance;
 
-  /*
-  RegisterClassA(
-  [in] const WNDCLASSA *lpWndClass
+  // Then we register the class
+  RegisterClassA(&window_class);
+
+  // Then we create the window
+  HWND window = CreateWindowExA(
+    0,
+    window_class.lpszClassName,
+    "BreakOut",
+    WS_VISIBLE | WS_OVERLAPPEDWINDOW,
+    CW_USEDEFAULT,
+    CW_USEDEFAULT,
+    1280,
+    720,
+    0, 0, 0, 0
 );
 
-  HWND window = CreateWindowExA(0, window_class.lpszClassName,
-  LPCSTR    lpWindowName,
-  DWORD     dwStyle,
-  int       X,
-  int       Y,
-  int       nWidth,
-  int       nHeight,
-  HWND      hWndParent,
-  HMENU     hMenu,
-  HINSTANCE hInstance,
-  LPVOID    lpParam
-);
-  */
+  // Our main loop
+  while (running) {
+    // Input
+    // Simulation
+    // Render
+  }
 
 };
 
